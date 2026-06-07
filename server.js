6c9491aa1e6ad26f03e9aea997dacce3a5cc5aff
Worawut Chusri <at3biz@gmail.com>
2026-06-07 17:10:05 +0700

    Update CORS configuration to allow specific origins and methods

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ const categoryRoutes = require('./routes/category');
 app.use(moregan('dev'));
 app.use(express.json({limit:'50mb'}));
 app.use(cors());
+// app.use(cors({
+//   origin: 'http://localhost:3000', // Allow requests from this origin
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+// }));
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://ecom2026-api-ten.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Auto load routes
 readdirSync('./routes')
