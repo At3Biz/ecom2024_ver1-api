f42f676b476e0196b0d7fc9c71b7d6f1be02d8c2
Worawut Chusri <at3biz@gmail.com>
2026-06-11 23:09:37 +0700

    Refactor CORS middleware configuration in server.js to enable specific origin and credentials

--- server.js
+++ server.js
@@ -11,7 +11,15 @@ const categoryRoutes = require('./routes/category');
 // Middleware to log requests
 app.use(moregan('dev'));
 app.use(express.json({limit:'50mb'}));
-app.use(cors());
+// app.use(cors());
+
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true // Enable if you send cookies/auth tokens
+}));
+
+
 // app.use(cors({
 //   origin: 'http://localhost:3000', // Allow requests from this origin
 //   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
