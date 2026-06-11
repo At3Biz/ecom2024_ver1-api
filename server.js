76b371d63b32f3f06b2a3c7e1cb5b8f401f95d14
Worawut Chusri <at3biz@gmail.com>
2026-06-11 23:04:39 +0700

    Enable CORS middleware in server.js and comment out previous configurations

--- server.js
+++ server.js
@@ -11,18 +11,19 @@ const categoryRoutes = require('./routes/category');
 // Middleware to log requests
 app.use(moregan('dev'));
 app.use(express.json({limit:'50mb'}));
-//app.use(cors());
+app.use(cors());
 // app.use(cors({
 //   origin: 'http://localhost:3000', // Allow requests from this origin
 //   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
 //   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
 // }));
 
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://ecom2024-ver1-api.vercel.app'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// app.use(cors({
+//   origin: ['http://localhost:5173', 'https://ecom2024-ver1-api.vercel.app'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
 
 // Auto load routes
 readdirSync('./routes')
