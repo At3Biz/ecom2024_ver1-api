841453a60899603d92aa9a74b38776a7299fef74
Worawut Chusri <at3biz@gmail.com>
2026-06-11 23:14:58 +0700

    Enable CORS middleware in server.js for all origins

--- server.js
+++ server.js
@@ -11,13 +11,8 @@ const categoryRoutes = require('./routes/category');
 // Middleware to log requests
 app.use(moregan('dev'));
 app.use(express.json({limit:'50mb'}));
-// app.use(cors());
+app.use(cors());
 
-
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true // Enable if you send cookies/auth tokens
-}));
 
 
 // app.use(cors({
